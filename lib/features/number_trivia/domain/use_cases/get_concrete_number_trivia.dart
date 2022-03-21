2ffdf616715604e09e1c2e9bbe944f2cd9b6e85f
SabianF <58588133+SabianF@users.noreply.github.com>
2022-03-21 13:07:41 -0700

    feat: red: added getConcreteNumberTrivia fn

--- lib/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/use_cases/get_concrete_number_trivia.dart
@@ -1,4 +1,9 @@
 
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../core/error/failures.dart';
+import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
 class GetConcreteNumberTrivia {
@@ -6,4 +11,11 @@ class GetConcreteNumberTrivia {
   const GetConcreteNumberTrivia(this.repository);
 
   final NumberTriviaRepository? repository;
+
+  Future<Either<Failure?, NumberTrivia?>?> execute({
+    @required int? number,
+  }) async {
+
+    return await repository?.getConcreteNumberTrivia(number);
+  }
 }
